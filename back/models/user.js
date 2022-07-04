c70e0c6f97facab77b0d5cc607a08fc58e871e17
Romain ROGUET <romain.roguet@gmail.com>
2022-07-04 12:23:07 +0200

    ajoute le commentaire

--- back/models/user.js
+++ back/models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
+
+// plugin qui empêche les doublons d'adresse mail 
 const uniqueValidator = require('mongoose-unique-validator')
 
-//création du schéma utilisateur
-//utilisation mongoose-unique-validator pour éviter d'avoir plus d'un utilisateur avec une adresse e-mail spécifique
+//définit le schéma de données d'un utilisateur pour rendre les entrés dans la BDD plus strictes
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
 
-//appliquer le validateur au schéma utilisateur
+//utilise le plugin pour s'assurer que l'adresse mail est unique
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
